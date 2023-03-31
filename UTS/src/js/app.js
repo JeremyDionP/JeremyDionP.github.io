08cae6030be9eff0a1f5899e31d019fe22086e85
JeremyDionP <jeremydionp@gmail.com>
2023-03-31 09:58:35 +0700

    fix

--- UTS/src/js/app.js
+++ UTS/src/js/app.js
@@ -1,13 +1,6 @@
+navigator.serviceWorker.register('/sw.js');
+
 var deferredPrompt;
-
-if ('serviceWorker' in navigator) {
-  navigator.serviceWorker
-    .register('/sw.js')
-    .then(function() {
-      console.log('Service worker registered!');
-    });
-}
-
 window.addEventListener('beforeinstallprompt', function(event) {
   console.log('beforeinstallprompt fired');
   event.preventDefault();
